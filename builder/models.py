782628a19826adf81f238ed1bca0434e731d4e82
abhishekkumar008 <abhishek.kumar@hsv.digital>
2022-06-18 11:14:58 +0530

    added chnages for save

--- builder/models.py
+++ builder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pages(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    html = models.TextField()
+    css = models.TextField()
+
--- builder/views.py
+++ builder/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Pages
+from django.core.serializers import serialize
+import json
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def savePage(request):
+    if(request.method=='POST'):
+        html = request.POST['html']
+        css = request.POST['css']
+        page = Pages.objects.create(name="untitled", html=html, css=css)
+        page.save()
+    return JsonResponse({ "result" : (json.loads(serialize('json', [page])))[0]}) 
